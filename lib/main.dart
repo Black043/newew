a751182cf18f31f266194d9fc1fa9494fd951174
Manjeet <ankitdeswal222@gmail.com>
2023-08-02 16:07:25 +0530

    delivery App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:newew/HomePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My HomePage",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      darkTheme: ThemeData.light(),
+      home: HomePage(),
+    );
+  }
+}
